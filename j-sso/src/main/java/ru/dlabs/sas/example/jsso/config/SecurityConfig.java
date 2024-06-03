a04d043befd322786d54e614206d5fa88292ac51
div <div@justcommunication.ru>
2024-06-03 16:19:51 +1000

    Упрощена настройка userdetailsservice в SecurityConfig

--- j-sso/src/main/java/ru/dlabs/sas/example/jsso/config/SecurityConfig.java
+++ j-sso/src/main/java/ru/dlabs/sas/example/jsso/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package ru.dlabs.sas.example.jsso.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -30,9 +29,10 @@ public class SecurityConfig {
                         .userInfoEndpoint(customizer -> customizer.userService(customOAuth2UserService)))
                 .formLogin(withDefaults())
                 .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().authenticated());
-        http.getSharedObject(AuthenticationManagerBuilder.class)
+                        .anyRequest().authenticated())
                 .userDetailsService(userDetailService);
+//        http.getSharedObject(AuthenticationManagerBuilder.class)
+//                .userDetailsService(userDetailService);
         return http.build();
     }
 }
